f381a5d4c56ece7b139ab0d95e29390dc6476488
Florian LAINE <florian.laine@epita.fr>
2025-08-14 00:20:08 +0200

    feat: add custom mkdocs-section-index

--- run_local.sh
+++ run_local.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd $(dirname $0)
+set -e
 python -m venv ./venv
 source ./venv/bin/activate
 pip install mkdocs-material
@@ -7,6 +8,7 @@ pip install mkdocs-roamlinks-plugin
 pip install mkdocs-rss-plugin
 pip install mkdocs-pdf
 pip install mkdocs-callouts
-pip install mkdocs-blogging-plugin
+# pip install mkdocs-section-index
+pip install -e ./mkdocs-section-index
 mkdocs serve --dirty
 # mkdocs gh-deploy
